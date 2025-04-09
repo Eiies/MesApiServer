569ed2597bfa70a6aa67640b851e30d533eae9d7
eiies <da.weii@outlook.com>
2025-04-09 22:45:06 +0800

    删除

--- obj/Debug/net9.0/MesApiServer.AssemblyInfo.cs
+++ obj/Debug/net9.0/MesApiServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MesApiServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99e3f6131e16c23e0ad4d79a9a3f0794889c977f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MesApiServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MesApiServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
